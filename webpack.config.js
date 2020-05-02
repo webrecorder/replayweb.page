6f04588dfda3658d5d397ad8ede2a8565ff5a409
Ilya Kreymer <ikreymer@gmail.com>
2020-05-02 13:27:17 -0700

    add CNAME, add examples from wabac.js, build directly from wabac.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = (env, argv) => {
     mode: 'production',
     entry: {
       'frontend': './src/frontend.js',
-      'sw': './src/sw.js',
+      'sw': 'wabac/src/sw.js',
       'wombat': 'wombat/src/wbWombat.js',
     },
 
