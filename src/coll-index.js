a711287af0d72b396f5057f56e59c99dc3f68e9b
Rebecca Cremona <rcremona@law.harvard.edu>
2020-09-02 17:47:11 -0400

    Don't use NAV landmark for non-nav; add heading for page structure.

--- src/coll-index.js
+++ src/coll-index.js
@@ -256,9 +256,9 @@ class CollIndex extends LitElement
           <span>${this.hideHeader ? 'Show ' : 'Hide'} <span class="is-sr-only">Header</span></span>
         </button>
       </div>
-      <nav class="panel is-light">
-        <div class="panel-heading"><span>Loaded Archives</span>
-        </div>
+      <div class="panel is-light">
+        <h2 class="panel-heading"><span>Loaded Archives</span>
+        </h2>
 
         ${this.colls.length ? html`
         <div class="panel-block sort-header is-small">
@@ -309,7 +309,7 @@ class CollIndex extends LitElement
           <i>No Archives so far! Archives loaded in the section above will appear here.</i>
         </div>
         `}
-      </nav>
+      </div>
     </section>
     `;
   }
