271910d5276e583d4ede15ea82d0707129d7ee6e
sua yoo <sua@suayoo.com>
2023-08-16 17:46:14 -0700

    extend prettierconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,10 @@ module.exports = {
         "browser": true,
         "es6": true
     },
-    "extends": "eslint:recommended",
+    "extends": [
+        "eslint:recommended",
+        "prettier"
+    ],
     "parserOptions": {
         "ecmaVersion": 2018,
         "sourceType": "module"
