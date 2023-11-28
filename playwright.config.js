2f683377f9652f85db13d78481b9f5cfc844d120
sua yoo <sua@suayoo.com>
2023-11-28 13:14:16 -0800

    fix eslint playwright

--- playwright.config.js
+++ playwright.config.js
@@ -1,24 +1,25 @@
-import { defineConfig } from '@playwright/test';
+/* eslint-env node */
+import { defineConfig } from "@playwright/test";
 export default defineConfig({
-  reporter: process.env.CI ? 'github' : 'list',
+  reporter: process.env.CI ? "github" : "list",
   webServer: [
     {
-      command: 'yarn run start-prod',
-      url: 'http://127.0.0.1:9990',
+      command: "yarn run start-prod",
+      url: "http://127.0.0.1:9990",
       timeout: 120 * 1000,
       reuseExistingServer: !process.env.CI,
     },
     {
-      command: 'yarn run test-start-embed',
-      url: 'http://127.0.0.1:8020',
+      command: "yarn run test-start-embed",
+      url: "http://127.0.0.1:8020",
       timeout: 120 * 1000,
       reuseExistingServer: !process.env.CI,
     },
     {
-      command: 'yarn run test-start-sandbox',
-      url: 'http://127.0.0.1:8030',
+      command: "yarn run test-start-sandbox",
+      url: "http://127.0.0.1:8030",
       timeout: 120 * 1000,
       reuseExistingServer: !process.env.CI,
-    }
-  ]
+    },
+  ],
 });
