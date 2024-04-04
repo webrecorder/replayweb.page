e76f9a46704049746a5de2a7f92c635a1d783824
Henry Wilkinson <henry@wilkinson.graphics>
2024-04-04 16:19:54 -0400

    Updates copy button icon

--- src/components/labeled-field.ts
+++ src/components/labeled-field.ts
@@ -48,7 +48,9 @@ class LabeledField extends LitElement {
       <div class="col-content">
         <slot></slot>
         ${this.copy
-          ? html` <sl-copy-button .value=${this.copy || ""}></sl-copy-button>`
+          ? html`<sl-copy-button .value=${this.copy || ""}>
+              <sl-icon slot="copy-icon" library="fa" name="far-clone"></sl-icon>
+            </sl-copy-button>`
           : nothing}
       </div>`;
   }
