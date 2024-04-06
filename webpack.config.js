65091bbb72af3f87abf1629c7f130855801d9969
Henry Wilkinson <henry@wilkinson.graphics>
2024-04-06 03:41:15 -0400

    Copy favicons to site dir on build

--- webpack.config.js
+++ webpack.config.js
@@ -111,11 +111,8 @@ const browserConfig = (/*env, argv*/) => {
 
   //const patterns = [{ from: "package.json", to: "_data/package.json" }];
   let patterns = [{
-    from: "build/icon.png",
-    to: "site/assets/favicon.png",
-  }, {
-    from: "assets/logo.svg",
-    to: "site/assets/logo.svg"
+    from: "src/assets/favicons",
+    to: "site",
   }];
 
   if (isDevServer) {
