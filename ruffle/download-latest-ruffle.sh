5d7ed5c886bd7ad2219ba284f44116627a9d9e39
emma <hi@emma.cafe>
2024-12-23 15:55:03 -0500

    clean up ruffle script a bit

--- ruffle/download-latest-ruffle.sh
+++ ruffle/download-latest-ruffle.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 RUFFLE_DIR=$SCRIPT_DIR
 
-SELFHOST_URL=$(curl "https://api.github.com/repos/ruffle-rs/ruffle/releases" | jq -r '.[0].assets[] | select(.name | contains("selfhosted")) | .browser_download_url')
+SELFHOST_URL=$(curl -s "https://api.github.com/repos/ruffle-rs/ruffle/releases" | jq -r '.[0].assets[] | select(.name | contains("selfhosted")) | .browser_download_url')
 
 echo "$SELFHOST_URL"
 
-curl -L -o $RUFFLE_DIR/ruffle.zip "$SELFHOST_URL"
+curl -L -o "$RUFFLE_DIR/ruffle.zip" "$SELFHOST_URL"
 
-rm $RUFFLE_DIR/*.js $RUFFLE_DIR/*.wasm
+rm "$RUFFLE_DIR/*.js" "$RUFFLE_DIR/*.wasm"
 
-cd $RUFFLE_DIR
-unzip $RUFFLE_DIR/ruffle.zip *.js *.wasm
+cd "$RUFFLE_DIR" || exit 1
+unzip "$RUFFLE_DIR/ruffle.zip" "*.js" "*.wasm"
 
-rm $RUFFLE_DIR/ruffle.zip
+rm "$RUFFLE_DIR/ruffle.zip"
