25d2903f27e9ad92ea0a20752c9aacd7b8ef334e
Ilya Kreymer <ikreymer@gmail.com>
2025-03-11 14:04:17 -0700

    add basic banner and GovArchiveBanner for custom override

--- src/proxyreplay/main.ts
+++ src/proxyreplay/main.ts
@@ -1,11 +1,12 @@
-import { addBanner } from "./banner";
+import { GovArchiveBanner } from "./banner";
 import { addInit } from "./app";
 
 if (
+  //disable for testing
   //navigator.serviceWorker.controller &&
   (document.currentScript as HTMLScriptElement).src.endsWith("banner=1")
 ) {
-  addBanner();
+  GovArchiveBanner.addBanner("gov-archive-banner", GovArchiveBanner);
 } else {
   addInit();
 }
