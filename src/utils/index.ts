3642096729dccedfe97d0fc2a41f15186ecb6c5b
emma <hi@emma.cafe>
2025-03-12 17:21:19 -0400

    remove unnecessary exports

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,11 +1,4 @@
-import rwpIcon from "~assets/icons/replaywebpage.svg";
-import rwpLogoAnimated from "~assets/brand/replaywebpage-icon-color-animated.svg";
-
 export * from "./dateTimeFormatter";
 export * from "./assertNonNullish";
 export * from "../pageutils";
-export * from "../misc";
-
 export { SWManager, serviceWorkerActivated } from "../swmanager";
-
-export { rwpIcon, rwpLogoAnimated };
