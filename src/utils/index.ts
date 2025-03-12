e9a63d207c85106223876ed548f0bc8ece0cc847
Ilya Kreymer <ikreymer@gmail.com>
2025-03-12 14:13:51 -0700

    add missing file

---
+++ src/utils/index.ts
@@ -0,0 +1,11 @@
+import rwpIcon from "~assets/icons/replaywebpage.svg";
+import rwpLogoAnimated from "~assets/brand/replaywebpage-icon-color-animated.svg";
+
+export * from "./dateTimeFormatter";
+export * from "./assertNonNullish";
+export * from "../pageutils";
+export * from "../misc";
+
+export { SWManager, serviceWorkerActivated } from "../swmanager";
+
+export { rwpIcon, rwpLogoAnimated };
