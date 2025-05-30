b6f26c19abb6308e017a68599efe45ee451cab18
Ilya Kreymer <ikreymer@gmail.com>
2025-05-30 12:30:43 -0700

    format, add changelist

--- playwright.config.js
+++ playwright.config.js
@@ -6,15 +6,15 @@ export default defineConfig({
   projects: [
     {
       name: "chrome",
-      use: { ...devices['Desktop Chrome'], channel: "chrome" },
+      use: { ...devices["Desktop Chrome"], channel: "chrome" },
     },
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
     },
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
     },
   ],
   webServer: [
